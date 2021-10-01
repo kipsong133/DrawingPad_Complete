cf69c6fcda87f412950bc164e9f7595dfe562f5f
KimWooSun <kipsong133@gmail.com>
2021-10-01 12:51:03 +0900

    Initial Commit

---
+++ DrawingPad_Complete/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DrawingPad_Complete
+//
+//  Created by 김우성 on 2021/10/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
